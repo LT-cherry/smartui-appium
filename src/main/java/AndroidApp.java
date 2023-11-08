cbd2270b30de57a7105f7d3768689c7b7d11b532
blu3r0se <fsusert1234@gmail.com>
2023-11-08 20:41:15 +0800

    first commit

---
+++ src/main/java/AndroidApp.java
@@ -0,0 +1,65 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import java.net.URL;
+import java.util.List;
+
+public class AndroidApp {
+
+    String userName = System.getenv("LT_USERNAME") == null ?
+            "username" : System.getenv("LT_USERNAME"); //Add username here
+    String accessKey = System.getenv("LT_ACCESS_KEY") == null ?
+            "accessKey" : System.getenv("LT_ACCESS_KEY"); //Add accessKey here
+
+    public String gridURL = "@mobile-hub.lambdatest.com/wd/hub";
+
+    AppiumDriver driver;
+
+    @Test
+    @org.testng.annotations.Parameters(value = {"device", "version", "platform"})
+    public void AndroidApp1(String device, String version, String platform) {
+        try {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("build","Java TestNG Android");
+            capabilities.setCapability("name",platform+" "+device+" "+version);
+            capabilities.setCapability("deviceName", device);
+            capabilities.setCapability("platformVersion",version);
+            capabilities.setCapability("platformName", platform);
+            capabilities.setCapability("isRealMobile", true);
+            //AppURL (Create from Wikipedia.apk sample in project)
+            capabilities.setCapability("app", "lt://proverbial-android"); //Enter your app url
+            capabilities.setCapability("deviceOrientation", "PORTRAIT");
+            capabilities.setCapability("console", true);
+            capabilities.setCapability("network", false);
+            capabilities.setCapability("visual", true);
+            capabilities.setCapability("devicelog", true);
+            capabilities.setCapability("smartUI.project", "real-device-android-java-testng");
+            capabilities.setCapability("smartUI.build", "build-1");
+
+            String hub = "https://" + userName + ":" + accessKey + gridURL;
+            driver = new AppiumDriver(new URL(hub), capabilities);
+
+            Thread.sleep(1000);
+            //Capture smartui screenshot
+            driver.executeScript("smartui.takeScreenshot=first");
+            Thread.sleep(5000);
+            driver.quit();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            try{
+                driver.quit();
+            }catch(Exception e1){
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+}
